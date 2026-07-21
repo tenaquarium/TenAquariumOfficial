be3bd49d1d3a7b6dd29865214c7c55a3894c06de
tenaquarium <tenaquarium@gmail.com>
2026-07-21 17:12:24 +0530

    gitchanges

--- index.js
+++ index.js
@@ -59,3 +59,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
+
+module.exports = app;
