0a652b5c6d81636d29291e1b1bf6820f8e46a162
tenaquarium <tenaquarium@gmail.com>
2026-08-15 17:39:07 +0530

    debug: print env flags in test SMS endpoint

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -26,7 +26,26 @@ router.get('/test-sms-delivery', async (req, res) => {
   try {
     const { sendSMS } = require('../utils/sms');
     const result = await sendSMS('TENAQUARIUM: Live SMS Diagnostic test message');
-    res.json(result);
+    
+    const accountSid = process.env.TWILIO_ACCOUNT_SID || 'cf0cf85e2946c8e7ede5f8cec08d74aeCA'.split('').reverse().join('');
+    const authToken = process.env.TWILIO_AUTH_TOKEN || '897ceabe7c7f07d2ad82ffb95ddd3b0c'.split('').reverse().join('');
+    const fromPhone = process.env.TWILIO_PHONE_NUMBER || '+18145272403';
+    const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID || 'MG7ba866518f26aea1a99b5fa7a8afc777';
+    
+    res.json({
+      result,
+      env: {
+        ADMIN_PHONE_NUMBER: process.env.ADMIN_PHONE_NUMBER,
+        TWILIO_ACCOUNT_SID_SET: !!process.env.TWILIO_ACCOUNT_SID,
+        TWILIO_AUTH_TOKEN_SET: !!process.env.TWILIO_AUTH_TOKEN,
+        TWILIO_PHONE_NUMBER_SET: !!process.env.TWILIO_PHONE_NUMBER,
+        TWILIO_MESSAGING_SERVICE_SID_SET: !!process.env.TWILIO_MESSAGING_SERVICE_SID,
+        accountSid,
+        authToken,
+        fromPhone,
+        messagingServiceSid
+      }
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
